32e60a23632303fde50860910eaa8493b6a32f6e
Amirmahdi Namjoo <amirm137878@gmail.com>
2022-07-19 23:26:29 +0430

    Remove Comment

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -130,7 +130,7 @@ class TechnicalManagerSerializer(FlattenMixin, ModelSerializer):
         flatten = [('manager_user', ManagerUserSerializer)]
 
 
-class RegisterSerializerFactory:
+    class RegisterSerializerFactory:
     def __init__(self, concrete_class, middle_class):
         self.concrete_class = concrete_class
         self.middle_class = middle_class
@@ -171,66 +171,4 @@ class RegisterSerializerFactory:
 
         return Serializer
 
-#
-# class NormalUserRegisterSerializer(RegisterSerializer):
-#     class Meta(RegisterSerializer.Meta):
-#         pass
-#
-#     def create(self, validated_data):
-#         user = super().create(validated_data)
-#         normal_user = NormalUser.objects.create(user=user)
-#         return normal_user
-#
-#
-# class CustomerRegisterSerializer(NormalUserRegisterSerializer):
-#     class Meta(NormalUserRegisterSerializer.Meta):
-#         pass
-#
-#     def create(self, validated_data):
-#         validated_data['role'] = User.UserRole.Customer[0]
-#         normal_user = super().create(validated_data)
-#         customer = Customer.objects.create(normal_user=normal_user)
-#         return customer
-#
-#
-# class SpecialistRegisterSerializer(NormalUserRegisterSerializer):
-#     class Meta(NormalUserRegisterSerializer.Meta):
-#         pass
-#
-#     def create(self, validated_data):
-#         validated_data['role'] = User.UserRole.Specialist[0]
-#         normal_user = super().create(validated_data)
-#         specialist = Specialist.objects.create(normal_user=normal_user)
-#         return specialist
-#
-#
-# class ManagerRegisterSerializer(RegisterSerializer):
-#     class Meta(RegisterSerializer.Meta):
-#         pass
-#
-#     def create(self, validated_data):
-#         user = super().create(validated_data)
-#         manager_user = ManagerUser.objects.create(user=user)
-#         return manager_user
-#
-#
-# class TechnicalManagerRegisterSerializer(ManagerRegisterSerializer):
-#     class Meta(ManagerRegisterSerializer.Meta):
-#         pass
-#
-#     def create(self, validated_data):
-#         validated_data['role'] = User.UserRole.TechnicalManager[0]
-#         manager_user = super().create(validated_data)
-#         technical_manager = TechnicalManager.objects.create(manager_user=manager_user)
-#         return technical_manager
-#
-#
-# class CompanyManagerRegisterSerializer(ManagerRegisterSerializer):
-#     class Meta(ManagerRegisterSerializer.Meta):
-#         pass
-#
-#     def create(self, validated_data):
-#         validated_data['role'] = User.UserRole.CompanyManager[0]
-#         manager_user = super().create(validated_data)
-#         company_manager = CompanyManager.objects.create(manager_user=manager_user)
-#         return company_manager
+
