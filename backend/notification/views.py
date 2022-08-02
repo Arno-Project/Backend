8701d6f3df245bcee5c5f735a9b5f1100ed347c0
Amirmahdi Namjoo <amirm137878@gmail.com>
2022-08-02 21:38:56 +0430

    Add notification app

---
+++ backend/notification/views.py
@@ -0,0 +1,53 @@
+from django.http import JsonResponse
+from django.utils.translation import gettext_lazy as _
+# Create your views here.
+from knox.auth import TokenAuthentication
+from rest_framework.response import Response
+from rest_framework.status import HTTP_404_NOT_FOUND
+from rest_framework.views import APIView
+
+from notification.models import NotificationCatalogue, Notification
+from notification.serializers import NotificationSerializer
+
+
+class NotificationView(APIView):
+    authentication_classes = [TokenAuthentication]
+
+    def get(self, request, notification_id=''):
+        many = False
+        if notification_id:
+            notification = NotificationCatalogue().get_by_id(notification_id)
+            if notification.get_user() != request.user:
+                return Response({
+                    'error': _('Notification not found')
+                }, status=HTTP_404_NOT_FOUND)
+        else:
+            print("many")
+            notification = NotificationCatalogue().get_unread(request.user)
+            many = True
+
+        serializer = NotificationSerializer(notification, many=many)
+
+        return JsonResponse({"notifications": serializer.data})
+
+    def post(self, request, notification_id=''):
+        if notification_id:
+            ids = [int(notification_id)]
+        else:
+            ids = request.data.get("id")
+        id_list = list(map(int, ids.split(','))) if isinstance(ids, str) else (
+            [ids] if isinstance(ids, int) else (list(map(int, ids)) if isinstance(ids, list) else None))
+        objs = []
+        for id in id_list:
+            try:
+                notif = Notification.objects.get(pk=id)
+            except:
+                continue
+            if not notif:
+                continue
+            if notif.get_user() != request.user:
+                continue
+            notif.set_is_read(True)
+            objs.append(notif)
+        Notification.objects.bulk_update(objs, ['is_read'])
+        return Response({'ids': map(lambda x: x.id, objs)})
